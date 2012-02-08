7454c7c3689769b0e8aa5125c7e12e3465295c8c
Kyle Breton <kyle@tryplehelix.com>
2012-02-08 07:30:47 -0800

    Initial commit of the HelloWorld project

---
+++ HelloWorld/HelloWorld.cpp
@@ -0,0 +1,14 @@
+#include <iostream>
+using std::cout;
+using std::endl;
+using std::cin;
+
+int main(void)
+{
+	cout << "Hello World!" << endl;
+	cout << endl << endl;
+	cout << "Press Enter to Continue...";
+	cout.flush();
+	cin.get();
+	return 0;
+}
