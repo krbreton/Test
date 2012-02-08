fbd17510fc07a8a75cb82aaf0e69b62a75e59056
Kyle Breton <kyle@tryplehelix.com>
2012-02-08 07:59:01 -0800

    Added soem basic documentation to the project

--- HelloWorld/HelloWorld.cpp
+++ HelloWorld/HelloWorld.cpp
@@ -1,3 +1,5 @@
+// Copyright 2011 Tryple Helix Solutions, Inc.
+// All Rights Reserved
 #include <iostream>
 using std::cout;
 using std::endl;
@@ -5,10 +7,17 @@ using std::cin;
 
 int main(void)
 {
+	// Print out the main text
 	cout << "Hello World!" << endl;
+
+	// Give some space
 	cout << endl << endl;
+	
+	// Guidance for quitting
 	cout << "Press Enter to Continue...";
 	cout.flush();
 	cin.get();
+
+	// Outta here!
 	return 0;
 }
